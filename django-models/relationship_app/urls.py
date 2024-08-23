0bea3ce53d0d9dd6eb72427f3eb870096754d6b8
Katiso-Kurata <katisokurata@gmail.com>
2024-08-23 21:42:57 +0000

    login 1

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -14,3 +14,13 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('relationship_app/', include('relationship_app.urls')),
 ]
+
+#
+from django.urls import path
+from .views import UserLoginView, UserLogoutView, UserRegisterView
+
+urlpatterns = [
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', UserLogoutView.as_view(), name='logout'),
+    path('register/', UserRegisterView.as_view(), name='register'),
+]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -21,3 +21,20 @@ class LibraryDetailView(DetailView):
     template_name = 'library_detail.html'
     context_object_name = 'library'
 
+#
+
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
+
+class UserLoginView(LoginView):
+    template_name = 'login.html'
+
+class UserLogoutView(LogoutView):
+    template_name = 'logout.html'
+
+class UserRegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'register.html'
+    success_url = reverse_lazy('login')
