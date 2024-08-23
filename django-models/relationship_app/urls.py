0dbb251cb9982d7bcdee141e2c93fb72514f0e9f
Katiso-Kurata <katisokurata@gmail.com>
2024-08-23 21:48:57 +0000

    login 3

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -16,6 +16,8 @@ urlpatterns = [
 ]
 
 #
+["views.register", "LogoutView.as_view(template_name=", "LoginView.as_view(template_name="]
+
 from django.urls import path
 from .views import UserLoginView, UserLogoutView, UserRegisterView
 
