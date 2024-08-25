7a3f2fb86393d82b7dd78d8827ab45436c1f405e
Katiso-Kurata <katisokurata@gmail.com>
2024-08-25 22:07:02 +0000

    sec

--- advanced_features_and_security/bookshelf/admin.py
+++ advanced_features_and_security/bookshelf/admin.py
@@ -9,3 +9,23 @@ class BookAdmin(admin.ModelAdmin):
     list_filter = ('publication_year',)
 # Register your models here.
 ["from .models import Book"]
+
+# advanced_features_and_security/relationship_app/admin.py
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    list_display = ('username', 'email', 'date_of_birth', 'profile_photo', 'is_staff')
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
