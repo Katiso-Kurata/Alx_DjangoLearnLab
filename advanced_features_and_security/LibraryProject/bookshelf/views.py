c1f2723039ee3ede24f0cec77e9445b3101fff13
Katiso-Kurata <katisokurata@gmail.com>
2024-08-27 18:51:54 +0000

    admin test 2

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+["book_list"]
+
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import permission_required
 from django.http import HttpResponseForbidden
