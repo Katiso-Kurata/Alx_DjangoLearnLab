ab320ce0844ac0a46b694049b4c03663c78fe245
Katiso-Kurata <katisokurata@gmail.com>
2024-09-08 16:41:44 +0000

    authentication

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path, include
 from .views import BookListAPIView
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 from .views import BookViewSet
 
 # Initialize the router and register the BookViewSet
 router = DefaultRouter()
-router.register(r'books', BookViewSet)
+router.register(r'books', BookViewSet, basename='book')
 
 urlpatterns = [
     path("api/books/", BookListAPIView.as_view(), name="book_list_create"),
     path('', include(router.urls)),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
 
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework import generics
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from .models import Book
 from .serializers import BookSerializer
 
@@ -15,3 +16,11 @@ class BookListAPIView(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()  # QuerySet for all Book instances
     serializer_class = BookSerializer  # Serializer class for Book
+    permission_classes = [IsAuthenticated]
+#space
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            permission_classes = [IsAuthenticated]  # Any authenticated user can list or retrieve
+        else:
+            permission_classes = [IsAdminUser]  # Only admin users can create, update, or delete
+        return [permission() for permission in permission_classes]
