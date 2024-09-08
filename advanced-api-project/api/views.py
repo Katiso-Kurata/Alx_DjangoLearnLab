47374170886ade2609238fc04044be1246e8b7b5
Katiso-Kurata <katisokurata@gmail.com>
2024-09-08 19:56:32 +0000

    advanced 7

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -7,7 +7,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from .models import Book
 from .serializers import BookSerializer
 ["from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated"]
-
+["from django_filters import rest_framework"]
 # List View for retrieving all books
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
